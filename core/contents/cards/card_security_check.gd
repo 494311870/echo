76384bb4c3234a8c7290b20e80e7cbaf3af373ec
‘suoyike’ <494311870@qq.com>
2025-05-22 19:07:19 +0800

    新增：安检 修改：调整关卡顺序 修复：守卫死亡时没有正确掉落的bug

--- core/contents/cards/card_security_check.gd
+++ core/contents/cards/card_security_check.gd
@@ -1,15 +1,12 @@
 extends Card
 
 func can_pass(other_card: Card) -> bool:
-	return false
+	return true
 
 
 func interact(other_card: Card)-> void:
-	var weapon = other_card.get_meta("weapon", 0)
-	if weapon == 0:
+	if other_card.has_meta("weapon"):
 		view.get_tree().call_group("main_handler", "game_over")
 		return
-
-	other_card.set_meta("weapon", 0)
-	other_card.refresh()
+		
 	mark_resolved()
